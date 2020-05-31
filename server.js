aca004dcdaf626166bda1fc3c749123a9abe239b
Jason Chong <34180725+meljason@users.noreply.github.com>
2020-05-31 03:29:28 -0400

    add express validator

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connecting to the database
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //Defining routes
