47e0a21e9733aec1b19f36e9957c161040616d09
Jason Chong <34180725+meljason@users.noreply.github.com>
2020-06-08 16:43:33 -0400

    Fix typo

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 //serve static assets in production
-if (process.env.NODE_END === "production") {
+if (process.env.NODE_ENV === "production") {
   //set the static folder
   app.use(express.static("client/build"));
 
